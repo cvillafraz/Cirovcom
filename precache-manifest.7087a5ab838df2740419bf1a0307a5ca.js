dcca8dafb5369493c5c8f81e50de31cfa1068def
cvillafraz <ciro1200@gmail.com>
2019-07-05 12:14:08 -0400

    Updates

---
+++ precache-manifest.7087a5ab838df2740419bf1a0307a5ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b16735ab1a1abb0b96ab88bf8a7cf8",
+    "url": "/Cirovcom/index.html"
+  },
+  {
+    "revision": "28ec1928f4506373a3dc",
+    "url": "/Cirovcom/static/css/main.bcd7fe64.chunk.css"
+  },
+  {
+    "revision": "abb6d414280b5da28a44",
+    "url": "/Cirovcom/static/js/2.fc49a777.chunk.js"
+  },
+  {
+    "revision": "28ec1928f4506373a3dc",
+    "url": "/Cirovcom/static/js/main.2e7e5595.chunk.js"
+  },
+  {
+    "revision": "edfa28d39efa533a4209",
+    "url": "/Cirovcom/static/js/runtime~main.18f072aa.js"
+  },
+  {
+    "revision": "6f91bb26730656b6750abb679f1b151c",
+    "url": "/Cirovcom/static/media/io.6f91bb26.png"
+  },
+  {
+    "revision": "afe9cc925a44559d5560a3135971a8a6",
+    "url": "/Cirovcom/static/media/logo.afe9cc92.png"
+  }
+]);
